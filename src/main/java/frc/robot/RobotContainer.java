73f6595fbcfa30b4641f7c3864606c6f1fcf642d
AnanyaLin <ananya.lingamallu8@gmail.com>
2026-01-23 19:13:43 -0500

    hb

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,26 +14,26 @@ import frc.robot.Subsystems.Shooter.Shooter;
 import frc.robot.Subsystems.Vision.Vision;
 
 public class RobotContainer {
-    private final Intake intake;
-    private final Shooter shooter;
-    private final Climber climber;
-    private final Hopper hopper;
+    //private final Intake intake;
+    //private final Shooter shooter;
+    //private final Climber climber;
+    //private final Hopper hopper;
 
-    private final Drive drive;
-    private final Vision limelightExample;
+    //private final Drive drive;
+    //private final Vision limelightExample;
 
     private final XboxController driverController = new XboxController(0);
-    private final XboxController operatorController = new XboxController(1);
+    //private final XboxController operatorController = new XboxController(1);
 
-    private final Alert primaryDisconnected = new Alert("Primary controller disconnected (port 0).",
-            AlertType.kWarning);
-    private final Alert secondaryDisconnected = new Alert("Secondary controller disconnected (port 1).",
-            AlertType.kWarning);
-    private final Alert overrideDisconnected = new Alert("Override controller disconnected (port 5).", AlertType.kInfo);
+    //private final Alert primaryDisconnected = new Alert("Primary controller disconnected (port 0).",
+            //AlertType.kWarning);
+    //private final Alert secondaryDisconnected = new Alert("Secondary controller disconnected (port 1).",
+            //AlertType.kWarning);
+    //private final Alert overrideDisconnected = new Alert("Override controller disconnected (port 5).", AlertType.kInfo);
     
-    private final LoggedDashboardChooser<Command> autoChooser;
+    //private final LoggedDashboardChooser<Command> autoChooser;
 
     public RobotContainer() {
-
+        
     }
 }
